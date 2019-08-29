fbe7ebbb153ad49664d520663e7808c21bc429b9
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-29 10:14:00 +0200

    Brew formula update for carnot version v0.3.0

--- carnot.rb
+++ carnot.rb
@@ -1,9 +1,9 @@
 class Carnot < Formula
   desc "Small utility to copy/move files from a source directory to a target directory and organize them by date"
   homepage "https://github.com/wejustdostuff/carnot"
-  url "https://github.com/wejustdostuff/carnot/releases/download/v0.2.2/carnot_0.2.2_Darwin_x86_64.tar.gz"
-  version "0.2.2"
-  sha256 "6c8f59c0908a9a7c2cd2979a6b13311aee1596a164bfc7ac28325ceb3b1a7e49"
+  url "https://github.com/wejustdostuff/carnot/releases/download/v0.3.0/carnot_0.3.0_Darwin_x86_64.tar.gz"
+  version "0.3.0"
+  sha256 "f33fe83b53af81f451b5ea8373b79d9b332ca6e13a9f7c7b1b1d4f3a0c3bfb85"
 
   def install
     bin.install "carnot"
